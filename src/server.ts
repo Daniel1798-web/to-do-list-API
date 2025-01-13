31fdecd2707640d37c07a2378b8c8292fc473547
Daniel Oto Diaz <danieloto212@gmail.com>
2025-01-13 16:06:27 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ connectDB();
 
 // Cors configuration
 
-const allowedOrigins = ['http://localhost:4200', 'https://to-do-list-front-roan.vercel.app/login'];
+const allowedOrigins = ['http://localhost:4200', 'https://to-do-list-api-production-28bb.up.railway.app/'];
 
 //update cors when uploading project to cloud
 
